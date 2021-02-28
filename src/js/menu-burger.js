406934d840fe795b4c0a1aa199f8a324e7d04543
Eduard Batiushchenko <72995778+Eduard-Batiushchenko@users.noreply.github.com>
2021-02-28 23:29:02 +0200

    complete full logic for burger-menu, add hero markup without logic

--- src/js/menu-burger.js
+++ src/js/menu-burger.js
@@ -1,14 +1,36 @@
-const hamburgerBtnRef = document.querySelector('.hamburger');
-const headerNavigationRef = document.querySelector('.header-navigation');
+if (screen.width <= 767) {
+  const hamburgerBtnRef = document.querySelector('.hamburger');
+  const headerNavigationRef = document.querySelector('.header-navigation');
+  const heroRef = document.querySelector('.hero');
 
-hamburgerBtnRef.addEventListener('click', onToggleBurger);
+  hamburgerBtnRef.addEventListener('click', onToggleBurger);
+  headerNavigationRef.addEventListener('click', onClickNav);
+  heroRef.addEventListener('click', onHeroClick);
 
-function onToggleBurger() {
-  hamburgerBtnRef.classList.toggle('is-active');
+  function onToggleBurger(e) {
+    hamburgerBtnRef.classList.toggle('is-active');
 
-  if (hamburgerBtnRef.classList.contains('is-active')) {
-    headerNavigationRef.classList.add('is-active');
-  } else {
-    headerNavigationRef.classList.remove('is-active');
+    if (hamburgerBtnRef.classList.contains('is-active')) {
+      headerNavigationRef.classList.add('is-open');
+    } else {
+      headerNavigationRef.classList.remove('is-open');
+    }
+  }
+
+  function onCloseMenu() {
+    hamburgerBtnRef.classList.remove('is-active');
+    headerNavigationRef.classList.remove('is-open');
+  }
+
+  function onClickNav(e) {
+    if (e.target !== headerNavigationRef) {
+      onCloseMenu();
+    }
+  }
+
+  function onHeroClick(e) {
+    if (e.currentTarget) {
+      onCloseMenu();
+    }
   }
 }
