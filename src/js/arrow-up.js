ce24a715d3fc1e8a465e8edcb05ec9f86514a527
Eduard Batiushchenko <72995778+Eduard-Batiushchenko@users.noreply.github.com>
2021-05-24 17:26:25 +0300

    mobile murkup done with logic

---
+++ src/js/arrow-up.js
@@ -0,0 +1,19 @@
+const arrowUpRef = document.querySelector('.arrow-up');
+
+window.addEventListener('scroll', throttle(listArrowBtn, 200));
+arrowUpRef.addEventListener('click', scrollToHeader);
+
+function scrollToHeader() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+function listArrowBtn() {
+  if (pageYOffset > 400) {
+    arrowUpRef.classList.add('is-active');
+  } else {
+    arrowUpRef.classList.remove('is-active');
+  }
+}
